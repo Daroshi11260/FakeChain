af804099be6aa16fd1fb94e09dc33fee89ad691f
Daroshi11260 <rytaneja@gmail.com>
2023-02-22 10:16:59 -0800

    finished proof of work

--- src/main/java/org/example/Block.java
+++ src/main/java/org/example/Block.java
@@ -26,7 +26,7 @@ public class Block {
 
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
-        while(!hash.substring( 0, difficulty).equals(target)) {
+        while(!hash.substring(0, difficulty).equals(target)) {
             nonce++;
             hash = calculateHash();
         }
